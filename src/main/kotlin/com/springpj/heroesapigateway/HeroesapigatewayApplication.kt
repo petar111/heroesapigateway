ebf42e96d48064ed6d2e569811599982b94c41b7
petar111 <jpetar765@gmail.com>
2023-10-25 11:29:18 +0200

    Initialized Heroes API gateway.

---
+++ src/main/kotlin/com/springpj/heroesapigateway/HeroesapigatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.springpj.heroesapigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeroesapigatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<HeroesapigatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/springpj/heroesapigateway/HeroesapigatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springpj.heroesapigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeroesapigatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
