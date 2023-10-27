ee91bb5d69fe26b54c595b3d77e7474391830d5c
petar111 <jpetar765@gmail.com>
2023-10-27 13:32:51 +0200

    Added configuration.

---
+++ src/main/java/com/springpj/heroesapigateway/HeroesapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springpj.heroesapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesapigatewayApplication.class, args);
+	}
+
+}
